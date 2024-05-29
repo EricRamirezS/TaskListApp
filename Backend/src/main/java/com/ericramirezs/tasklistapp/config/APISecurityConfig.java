205a1ed2cfca887eef31e9df96fb520c9ad701ad
EricRamirezS <eramirezsantis@gmail.com>
2024-05-28 20:05:08 -0400

    Security API Key and Swagger Api Key

---
+++ Backend/src/main/java/com/ericramirezs/tasklistapp/config/APISecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ericramirezs.tasklistapp.config;
+
+import com.ericramirezs.tasklistapp.security.ApiKeyFilter;
+import com.ericramirezs.tasklistapp.security.UnauthorizedHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class APISecurityConfig {
+
+    private final ApiKeyFilter authFilter;
+    private final UnauthorizedHandler unauthorizedHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .exceptionHandling(configurer -> configurer.authenticationEntryPoint(unauthorizedHandler))
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**",
+                                "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
