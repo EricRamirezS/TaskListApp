9ee20399153091d79fae6fdcad728443dfbb1361
EricRamirezS <eramirezsantis@gmail.com>
2024-05-30 05:16:41 -0400

    SQL Script

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE "testDB";
+
+\c testDB
+
+CREATE TABLE IF NOT EXISTS public.tasks
+(
+    id BIGSERIAL NOT NULL ,
+    description character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    active boolean NOT NULL,
+    CONSTRAINT tasks_pkey PRIMARY KEY (id)
+);
